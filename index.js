8b56b525093f1609dba9916e5ecffbae015dc899
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-12 10:10:11 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messagesArray = []
+    for (let i = 0; i < names.length; i++){
+        messagesArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messagesArray
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
